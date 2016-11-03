6102dd97cad0b21948ccdd312384bec7d715740f
Gareth Johns <info@garethjohnsdesign.com>
2016-11-03 20:45:26 +0000

    New Layout

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -1,6 +1,6 @@
 $(document).foundation();
 
-$('body').removeClass('fade-out');
+// $('body').removeClass('fade-out');
 
 // Scroll
 $('a[href*="#"]:not([href="#"])').click(function() {
@@ -14,18 +14,6 @@ if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && lo
     return false;
   }
 }
-});
-
-// Carousel
-$('.carousel').flickity({
-  // options
-  contain: true,
-  setGallerySize: false,
-  wrapAround: true,
-  autoPlay: true,
-  prevNextButtons: true,
-  pageDots: false,
-  arrowShape: 'M100,46.875H11.9688l17.4688-17.4688L25,25L0,50l25,25l4.4062-4.4062L11.9688,53.125H100V46.875z'
 });
 
 
